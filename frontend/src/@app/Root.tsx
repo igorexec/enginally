0f6fd2755cb6278063824e18e9b0e8b36675a3e2
igorexec <icheliadinski@gmail.com>
2020-03-27 22:02:21 -0400

    chg: structure

---
+++ frontend/src/@app/Root.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Home} from '@pages/home';
+
+export const Root: FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ frontend/src/@pages/home/__test__/Home.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+test.todo('renders');
+
+// import { render } from '@testing-library/react';
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });

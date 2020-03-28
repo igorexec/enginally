e8429a862f609eff574439b191a5b5bf5302daa5
igorexec <icheliadinski@gmail.com>
2020-03-27 22:33:30 -0400

    add: structure for core

--- frontend/src/@app/Root.tsx
+++ frontend/src/@app/Root.tsx
@@ -1,13 +1,15 @@
-import React, {FC} from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {Home} from '@pages';
+import React, {FC} from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Home} from '@pages'
 
 export const Root: FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
-  );
+    <React.StrictMode>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
 }

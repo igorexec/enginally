671984f7c2a67be9a6a1fe73a92dbf58e8587812
igorexec <icheliadinski@gmail.com>
2020-03-27 22:08:31 -0400

    chg: structure

--- frontend/src/@app/Root.tsx
+++ frontend/src/@app/Root.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import {Home} from '@pages/home';
+import {Home} from '@pages';
 
 export const Root: FC = () => {
   return (

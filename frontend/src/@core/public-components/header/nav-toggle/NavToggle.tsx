41f32eef055f97184f3dd95f2fd93f5a2e7f48d2
igorexec <icheliadinski@gmail.com>
2020-03-31 14:56:57 -0400

    chg: nav toggle

--- frontend/src/@core/public-components/header/nav-toggle/NavToggle.tsx
+++ frontend/src/@core/public-components/header/nav-toggle/NavToggle.tsx
@@ -4,6 +4,5 @@ import styles from './NavToggle.module.scss'
 
 export const NavToggle = () => {
   const classes = styles['nav-toggle']
-
   return <span className={classes} data-testid="NavToggle" />
 }

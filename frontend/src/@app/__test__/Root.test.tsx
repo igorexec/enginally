beb736e356fa1745378a7aa21c1a8fb4a5afe5f9
igorexec <icheliadinski@gmail.com>
2020-04-01 16:44:35 -0400

    fix: tests

--- frontend/src/@app/__test__/Root.test.tsx
+++ frontend/src/@app/__test__/Root.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { render, getByTestId } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { render } from '@testing-library/react'
 import { mockMatchMedia } from '__mocks__/window'
-import { Router } from 'react-router-dom'
 
 import { Root } from '../Root'
 

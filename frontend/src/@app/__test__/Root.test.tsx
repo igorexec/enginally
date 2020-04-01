67bd907c3444687a5d769be896b10ece2b40a6ec
igorexec <icheliadinski@gmail.com>
2020-04-01 15:50:51 -0400

    chg: deps

--- frontend/src/@app/__test__/Root.test.tsx
+++ frontend/src/@app/__test__/Root.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByTestId } from '@testing-library/react'
+import { createMemoryHistory } from 'history'
 import { mockMatchMedia } from '__mocks__/window'
+import { Router } from 'react-router-dom'
 
 import { Root } from '../Root'
 

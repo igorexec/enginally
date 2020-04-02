460554240298d1f93f162484aeb7aeb61a3c256d
igorexec <icheliadinski@gmail.com>
2020-04-01 20:24:15 -0400

    add: styles for desktop

--- frontend/src/@core/public-components/header/Header.tsx
+++ frontend/src/@core/public-components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { Responsive } from '@core/ui-kit'
 import { navigationSelector } from '@core/navigation'
 import { ScreenSizes } from '@configs/design'
@@ -10,12 +11,15 @@ import styles from './Header.module.scss'
 export const Header = () => {
   const classes = styles['header']
   const titleClasses = styles['header__title']
+  const linkClasses = styles['header__link']
 
   const navItems = useSelector(navigationSelector)
 
   return (
     <header className={classes} data-testid="Header">
-      <h1 className={titleClasses}>Enginally</h1>
+      <h1 className={titleClasses}>
+        <Link to="/" className={linkClasses}>Enginally</Link>
+      </h1>
       <Responsive maxWidth={ScreenSizes.Large}>
         <MobileMenu navItems={navItems} />
       </Responsive>
